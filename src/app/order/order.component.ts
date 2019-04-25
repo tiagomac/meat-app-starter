c573e1bc56896aa140abb6beb5507a2a8cfee67f
Tiago Mesquita <tiagomac@gmail.com>
2019-04-25 08:51:06 -0300

    Validação de e-mail personalizada reactive form

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms'
 
 @Component({
   selector: 'mt-order',
@@ -38,7 +38,19 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', [Validators.required])
-    })
+    }, {validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if (!email || !emailConfirmation){
+      return undefined
+    }
+    if (email.value !== emailConfirmation.value){
+      return {emailsNotMatch:true}
+    }
+    return undefined
   }
 
   itemsValue(): number {
